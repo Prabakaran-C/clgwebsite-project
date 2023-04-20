6b106e26714dde783d285de8be369c3839b4ca81
Prabakaran-C <124548017+Prabakaran-C@users.noreply.github.com>
2023-04-20 13:51:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let burger = document.querySelector(".burger");
+let links = document.querySelector(".nav-links");
+let textarea= document.querySelector(".text-center");
+
+
+burger.addEventListener('click',()=> {
+    links.classList.toggle("nav-show");
+    textarea.classList.toggle("textareahide");
+})
---
+++ form.php
@@ -0,0 +1,17 @@
+<?php
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+mail(to,subject,body,header)
+
+// $from = 'forms.rivers.in'
+// $to ='prabakaran94422@gamail.com'
+// $body = "Firt Name: $fname.\n".
+//         "Last Name: $lname.\n".
+//         "email: $email.\n".
+//         "message: $message.\n".
+
+?> 
